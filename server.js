2faac28d2876885eab7ad7a1e380d05bc6da51d4
michal <michmiku@gmail.com>
2020-11-13 00:22:56 +0100

    changed database

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,18 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const usersRouter = require('./routes/users');
-const musicLibrary = require('./routes/music')
+const usersRouter = require("./routes/users");
+const musicLibrary = require("./routes/music");
 
-app.use('/users', usersRouter);
-app.use('/music', musicLibrary)
+app.use("/users", usersRouter);
+app.use("/music", musicLibrary);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
