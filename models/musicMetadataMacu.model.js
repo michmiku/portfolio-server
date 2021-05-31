9a3d92cdb36c10f9cfacaa35346a55e5183cc5f0
michal <michmiku@gmail.com>
2021-05-31 02:28:20 +0200

    macus music added

---
+++ models/musicMetadataMacu.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const musicMetadataSchema = new Schema(
+  {
+    file: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    rawDuration: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const MusicMetadata = mongoose.model("MusicMetadataMacu", musicMetadataSchema);
+
+module.exports = MusicMetadata;
--- routes/music.js
+++ routes/music.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 let Music = require("../models/music.model");
 let MusicMetadata = require("../models/musicMetadata.model");
+let MusicMetadataMacu = require("../models/musicMetadataMacu.model");
 const fs = require("fs");
 const path = require("path");
 const mm = require("music-metadata");
@@ -44,6 +45,43 @@ router.route("/").get((req, res) => {
         });
     });
   });
+  fs.readdir(__dirname + "/../music-macu", function (err, files) {
+    if (err) {
+      return console.log(err);
+    }
+    files.forEach(function (filename, key) {
+      mm.parseFile(__dirname + "/../music-macu/" + filename)
+        .then((metadata) => {
+          let duration = metadata.format.duration;
+          let minutes = Math.floor(duration / 60);
+          let seconds = Math.floor(duration - minutes * 60);
+          if (seconds < 10) {
+            seconds = "0" + seconds;
+          }
+          if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+          duration = minutes + ":" + seconds;
+          MusicMetadataMacu.find({ file: filename })
+            .then((music) => {
+              if (music.length === 0) {
+                const newMusicMetadataMacu = new MusicMetadataMacu({
+                  file: filename,
+                  title: metadata.common.title,
+                  artist: metadata.common.artist,
+                  duration: duration,
+                  rawDuration: metadata.format.duration,
+                });
+                newMusicMetadataMacu.save();
+              }
+            })
+            .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => {
+          console.error(err.message);
+        });
+    });
+  });
 });
 router.route("/getlist").get((req, res) => {
   MusicMetadata.find()
@@ -52,8 +90,25 @@ router.route("/getlist").get((req, res) => {
     })
     .catch((err) => res.status(400).json("Error: " + err));
 });
+router.route("/get-list-macu").get((req, res) => {
+  MusicMetadata.find()
+    .then((music) => {
+      res.json(music);
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 router.route("/list").get((req, res) => {
   fs.readdir(__dirname + "/../music", function (err, files) {
+    files.forEach(function (filename, key) {
+      res.write('<a href= "' + filename + '">' + filename + "</a><br>");
+      res.write("\n\n");
+    });
+    res.end();
+  });
+});
+
+router.route("/list-macu").get((req, res) => {
+  fs.readdir(__dirname + "/../music-macu", function (err, files) {
     files.forEach(function (filename, key) {
       res.write('<a href= "' + filename + '">' + filename + "</a><br>");
       res.write("\n\n");
@@ -104,18 +159,5 @@ router.route("/*.ogg").get((req, res) => {
     }
   );
 });
-router.route("/*.mp3").get((req, res) => {
-  fs.readFile(
-    __dirname + "/../music" + decodeURI(req.url),
-    function (err, data) {
-      if (!err) {
-        res.writeHead(200, { "Content-Type": "audio/mpeg" });
-        res.write(data);
-        res.end();
-      } else {
-        console.log(err);
-      }
-    }
-  );
-});
+
 module.exports = router;
